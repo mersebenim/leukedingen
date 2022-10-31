c6432af5dd8fc6dfda62c9b8f9746d30d8337a9a
mersebenim <0112261@zadkine.nl>
2022-10-31 15:45:40 +0100

    eerste versie

---
+++ eenofmeercss/test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+        $naam = "Webscan IT Campus";
+    ?>
+    
+        
+    <p>En hier staat nog veel meer HTML en zo</p>
+    
+        
+            
+                
+    <script>
+        var wiebenje = <?php echo $naam; ?>;
+        
+        alert("Welkom " + wiebenje);
+        
+    </script>    
+</body>
+</html>
